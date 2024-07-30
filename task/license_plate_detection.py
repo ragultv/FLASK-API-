c5c8dae8075749dcf493030e7891cc7e1df4ba54
ragul <141637129+ragultv@users.noreply.github.com>
2024-07-30 18:50:02 +0530

    Update license_plate_detection.py

--- task/license_plate_detection.py
+++ task/license_plate_detection.py
@@ -1,41 +1,52 @@
-
 from ultralytics import YOLO
-from flask import Flask, request, jsonify
-from PIL import Image
+from flask import Flask, send_file, request
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
+import io
+import cv2
+import pytesseract
 
-model = YOLO(r"C:\Users\tragu\Downloads\best.pt")
+model = YOLO(r"C:\Users\tragu\OneDrive\Documents\models\best.pt")
 
 app = Flask('__name__')
 
-
 def predict(img):
     results = model.predict(img)
+    return results
 
+def extract_license_plate_text(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    text = pytesseract.image_to_string(gray, config='--psm 8')
+    return text.strip()
 
 @app.route('/predict', methods=['POST'])
 def predict_route():
     if request.method == 'POST':
-        img = request.files.get('file')
-        converted = Image.open(img)
+        image = request.files.get('file')
+        converted = Image.open(image)
         imgarr = np.array(converted)
-        licence_plate = predict(imgarr)
-        # Example of processing the results (assuming the model outputs bounding boxes and labels)
-        result_data = []
-        for detection in licence_plate.xyxy:
-            bbox = detection[:4]  # bounding box coordinates
-            confidence = detection[4]  # confidence score
-            label = detection[5]  # class label
-            result_data.append({
-                'bbox': bbox.tolist(),
-                'confidence': confidence.item(),
-                'label': label.item()
-            })
+        results = predict(imgarr)
 
-        result = {'messsage': 'licence plate detected'}
-        return jsonify(result)
+        detection = results[0].boxes[0]
+        x1, y1, x2, y2 = map(int, detection.xyxy[0])
+        license_plate_img = imgarr[y1:y2, x1:x2]
+        license_plate_text = extract_license_plate_text(license_plate_img)
+        draw = ImageDraw.Draw(converted)
+        font_path = r"C:\Users\tragu\Downloads\Humor-Sans.ttf"
+        font = ImageFont.truetype(font_path, 24)
+        text_bbox = font.getbbox(license_plate_text)
+        text_width = text_bbox[2] - text_bbox[0]
+        text_height = text_bbox[3] - text_bbox[1]
+        text_x = x2 - text_width
+        text_y = y1 - text_height if y1 - text_height > 0 else y1
 
+        draw.rectangle(((text_x, text_y), (text_x + text_width, text_y + text_height)), fill="black")
+        draw.text((text_x, text_y), license_plate_text, fill="white", font=font)
+        draw.rectangle([x1, y1, x2, y2], outline="red", width=2)
+        img_io = io.BytesIO()
+        converted.save(img_io, 'JPEG')
+        img_io.seek(0)
+        return send_file(img_io, mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
